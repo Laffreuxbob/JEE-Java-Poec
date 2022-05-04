5decbc8ef51c899189e7bb129259ea0366a5a143
Unknown <olivier.bricaud@sfr.fr>
2022-05-04 10:36:42 +0200

    assertEquals

--- src/test/java/fr/epsi/rennes/poec/bob/service/AgeServiceUnitTest.java
+++ src/test/java/fr/epsi/rennes/poec/bob/service/AgeServiceUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class AgeServiceUnitTest {
 
@@ -19,6 +21,6 @@ public class AgeServiceUnitTest {
         int age = service.age(ageString);
 
         // then
-        Assert.assertEquals()
+        assertEquals(26, age);
     }
 }
